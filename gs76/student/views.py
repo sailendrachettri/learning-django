f46596e8a7634c9bcd9162fd1c87ebde750d049f
sailendrachettri <sailendra9083@gmail.com>
2021-04-07 06:30:38 +0530

    file bases session

---
+++ gs76/student/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse
+
+def setsession(request):
+    request.session['name'] = 'Sonam'
+    return render(request, 'student/setsession.html')
+
+def getsession(request):
+        name = request.session['name']
+        return render(request, 'student/getsession.html', {'name':name})
+
+def delsession(request):
+    request.session.flush()
+    request.session.clear_expired()
+    return render(request, 'student/delsession.html')
